7b57f764731d00cb5eaabae6cbd42581e7958876
meraz arman <merazarman890@gmail.com>
2020-08-27 14:15:25 +0600

    first commit

---
+++ src/fakeData/Course.js
@@ -0,0 +1,168 @@
+var course = [
+{
+    "key": 1,
+    "instructor": "mujahidul islam",
+    "title": "javascript full stack web development",
+	"image": ""
+    "url": "https://via.placeholder.com/600/92c952",
+    "thumbnailUrl": "https://via.placeholder.com/150/92c952",
+	"star":3,
+	"img":"https://images-na.ssl-images-amazon.com/images/I/51VCP05020L._AC_US218_.jpg"
+	
+  },
+  {
+    "key": 2,
+    "instructor": "mujahidul islam",
+    "title": "accusamus beatae ad facilis cum similique qui sunt",
+	"image": ""
+    "url": "https://via.placeholder.com/600/92c952",
+    "thumbnailUrl": "https://via.placeholder.com/150/92c952",
+	"star":3,
+	"img":"https://images-na.ssl-images-amazon.com/images/I/51VCP05020L._AC_US218_.jpg"
+	
+  },
+  {
+    "key": 3,
+    "instructor": "mujahidul islam",
+    "title": "accusamus beatae ad facilis cum similique qui sunt",
+	"image": ""
+    "url": "https://via.placeholder.com/600/92c952",
+    "thumbnailUrl": "https://via.placeholder.com/150/92c952",
+	"star":3,
+	"img":"https://images-na.ssl-images-amazon.com/images/I/51VCP05020L._AC_US218_.jpg"
+	
+  },
+  {
+    "key": 4,
+    "instructor": "mujahidul islam",
+    "title": "accusamus beatae ad facilis cum similique qui sunt",
+	"image": ""
+    "url": "https://via.placeholder.com/600/92c952",
+    "thumbnailUrl": "https://via.placeholder.com/150/92c952",
+	"star":3,
+	"img":"https://images-na.ssl-images-amazon.com/images/I/51VCP05020L._AC_US218_.jpg"
+	
+  },
+  {
+    "key": 5,
+    "instructor": "mujahidul islam",
+    "title": "accusamus beatae ad facilis cum similique qui sunt",
+	"image": ""
+    "url": "https://via.placeholder.com/600/92c952",
+    "thumbnailUrl": "https://via.placeholder.com/150/92c952",
+	"star":3,
+	"img":"https://images-na.ssl-images-amazon.com/images/I/51VCP05020L._AC_US218_.jpg"
+	
+  },
+  {
+    "key": 6,
+    "instructor": "mujahidul islam",
+    "title": "accusamus beatae ad facilis cum similique qui sunt",
+	"image": ""
+    "url": "https://via.placeholder.com/600/92c952",
+    "thumbnailUrl": "https://via.placeholder.com/150/92c952",
+	"star":3,
+	"img":"https://images-na.ssl-images-amazon.com/images/I/51VCP05020L._AC_US218_.jpg"
+	
+  },
+  {
+    "key": 7,
+    "instructor": "mujahidul islam",
+    "title": "accusamus beatae ad facilis cum similique qui sunt",
+	"image": ""
+    "url": "https://via.placeholder.com/600/92c952",
+    "thumbnailUrl": "https://via.placeholder.com/150/92c952",
+	"star":3,
+	"img":"https://images-na.ssl-images-amazon.com/images/I/51VCP05020L._AC_US218_.jpg"
+	
+  },
+  {
+    "key": 8,
+    "instructor": "mujahidul islam",
+    "title": "accusamus beatae ad facilis cum similique qui sunt",
+	"image": ""
+    "url": "https://via.placeholder.com/600/92c952",
+    "thumbnailUrl": "https://via.placeholder.com/150/92c952",
+	"star":3,
+	"img":"https://images-na.ssl-images-amazon.com/images/I/51VCP05020L._AC_US218_.jpg"
+	
+  },
+  {
+    "key": 9,
+    "instructor": "mujahidul islam",
+    "title": "accusamus beatae ad facilis cum similique qui sunt",
+	"image": ""
+    "url": "https://via.placeholder.com/600/92c952",
+    "thumbnailUrl": "https://via.placeholder.com/150/92c952",
+	"star":3,
+	"img":"https://images-na.ssl-images-amazon.com/images/I/51VCP05020L._AC_US218_.jpg"
+	
+  },
+  {
+    "key": 10,
+    "instructor": "mujahidul islam",
+    "title": "accusamus beatae ad facilis cum similique qui sunt",
+	"image": ""
+    "url": "https://via.placeholder.com/600/92c952",
+    "thumbnailUrl": "https://via.placeholder.com/150/92c952",
+	"star":3,
+	"img":"https://images-na.ssl-images-amazon.com/images/I/51VCP05020L._AC_US218_.jpg"
+	
+  },
+  {
+    "key": 11,
+    "instructor": "mujahidul islam",
+    "title": "accusamus beatae ad facilis cum similique qui sunt",
+	"image": ""
+    "url": "https://via.placeholder.com/600/92c952",
+    "thumbnailUrl": "https://via.placeholder.com/150/92c952",
+	"star":3,
+	"img":"https://images-na.ssl-images-amazon.com/images/I/51VCP05020L._AC_US218_.jpg"
+	
+  },
+  {
+    "key": 12,
+    "instructor": "mujahidul islam",
+    "title": "accusamus beatae ad facilis cum similique qui sunt",
+	"image": ""
+    "url": "https://via.placeholder.com/600/92c952",
+    "thumbnailUrl": "https://via.placeholder.com/150/92c952",
+	"star":3,
+	"img":"https://images-na.ssl-images-amazon.com/images/I/51VCP05020L._AC_US218_.jpg"
+	
+  },
+  {
+    "key": 13,
+    "instructor": "mujahidul islam",
+    "title": "accusamus beatae ad facilis cum similique qui sunt",
+	"image": ""
+    "url": "https://via.placeholder.com/600/92c952",
+    "thumbnailUrl": "https://via.placeholder.com/150/92c952",
+	"star":3,
+	"img":"https://images-na.ssl-images-amazon.com/images/I/51VCP05020L._AC_US218_.jpg"
+	
+  },
+  {
+    "key": 14,
+    "instructor": "mujahidul islam",
+    "title": "accusamus beatae ad facilis cum similique qui sunt",
+	"image": ""
+    "url": "https://via.placeholder.com/600/92c952",
+    "thumbnailUrl": "https://via.placeholder.com/150/92c952",
+	"star":3,
+	"img":"https://images-na.ssl-images-amazon.com/images/I/51VCP05020L._AC_US218_.jpg"
+	
+  },
+  {
+    "key": 15,
+    "instructor": "mujahidul islam",
+    "title": "accusamus beatae ad facilis cum similique qui sunt",
+	"image": ""
+    "url": "https://via.placeholder.com/600/92c952",
+    "thumbnailUrl": "https://via.placeholder.com/150/92c952",
+	"star":3,
+	"img":"https://images-na.ssl-images-amazon.com/images/I/51VCP05020L._AC_US218_.jpg"
+	
+  },
+
+]
